6b8394165e68ffdc63d83b4726300e5b08f6d921
ahmadzak0307 <ahmadzak0307@nusamandiri.ac.id>
2022-05-29 20:56:28 +0700

    Done Challenge 06 Logic Flow

--- index.js
+++ index.js
@@ -14,59 +14,85 @@
 const arrayObjectPegawai = require("./data-customer.json");
 
 function lakukanLooping(arrPegawai) {
-  // ! JANGAN DIMODIFIKASI
-  let dataYangAkanDilooping = arrPegawai;
+    // ! JANGAN DIMODIFIKASI
+    let dataYangAkanDilooping = arrPegawai;
 
-  /*
-    TODO 1: Buatlah sebuah variabel bernama "hasilLooping" 
-      yang berisi gabungan nama depan dan belakang dari masing masing pegawai
+    /*
+      TODO 1: Buatlah sebuah variabel bernama "hasilLooping" 
+        yang berisi gabungan nama depan dan belakang dari masing masing pegawai
 
-      Contoh: ["Aisyah Nirmala", "Mansur Faisal", ...]
-  */
-  let hasilLooping = null;
+        Contoh: ["Aisyah Nirmala", "Mansur Faisal", ...]
+    */
+    let hasilLooping = [];
+    for (let i = 0; i < dataYangAkanDilooping.length; i++) {
+        hasilLooping.push(dataYangAkanDilooping[i].namaDepan + " " + dataYangAkanDilooping[i].namaBelakang);
+    }
 
-  /*
-    TODO 2: Buatlah sebuah variabel bernama "jumlahPria"
-      yang berisi jumlah pria dari masing masing pegawai
-  */
-  let jumlahPria = null;
+    /*
+      TODO 2: Buatlah sebuah variabel bernama "jumlahPria"
+        yang berisi jumlah pria dari masing masing pegawai
+    */
+    let jumlahPria = null;
+    const tempjumlahPria = [];
+    for (let i = 0; i < dataYangAkanDilooping.length; i++) {
+        if (dataYangAkanDilooping[i].jenisKelamin === "M") {
+            tempjumlahPria.push(dataYangAkanDilooping[i].jenisKelamin)
+            jumlahPria = tempjumlahPria.length
+        }
+    }
 
-  /*
-    TODO 3: Buatlah sebuah variabel bernama "jumlahWanita"
-      yang berisi jumlah wanita dari masing masing pegawai
-  */
-  let jumlahWanita = null;
+    // console.log(tempjumlahPria);
+    /*
+      TODO 3: Buatlah sebuah variabel bernama "jumlahWanita"
+        yang berisi jumlah wanita dari masing masing pegawai
+    */
+    let jumlahWanita = null;
+    const tempjumlahWanita = [];
+    for (let i = 0; i < dataYangAkanDilooping.length; i++) {
+        if (dataYangAkanDilooping[i].jenisKelamin === "F") {
+            tempjumlahWanita.push(dataYangAkanDilooping[i].jenisKelamin)
+            jumlahWanita = tempjumlahWanita.length
+        }
+    }
 
-  /*
-    TODO 4: Buatlah sebuah variabel bernama "komentar"
-      yang akan mengomentari apakah lebih banyak Pria atau Wanita
-      
-      Apabila Pria lebih banyak dari Wanita, maka komentar akan berisi:
-        "Jumlah Pria lebih banyak dari Wanita"
-      Apabila Wanita lebih banyak dari Pria, maka komentar akan berisi:
-        "Jumlah Wanita lebih banyak dari Pria"
-      Apabila imbang, maka komentar akan berisi:
-        "Jumlah Pria dan Wanita berimbang"
-  */
-  let komentar = null;
+    /*
+      TODO 4: Buatlah sebuah variabel bernama "komentar"
+        yang akan mengomentari apakah lebih banyak Pria atau Wanita
+        
+        Apabila Pria lebih banyak dari Wanita, maka komentar akan berisi:
+          "Jumlah Pria lebih banyak dari Wanita"
+        Apabila Wanita lebih banyak dari Pria, maka komentar akan berisi:
+          "Jumlah Wanita lebih banyak dari Pria"
+        Apabila imbang, maka komentar akan berisi:
+          "Jumlah Pria dan Wanita berimbang"
+    */
+    let komentar = null;
+    if (jumlahPria > jumlahWanita) {
+        komentar = "Jumlah Pria lebih banyak dari Wanita"
+    } else if (jumlahPria < jumlahWanita) {
+        komentar = "Jumlah Wanita lebih banyak dari Pria"
+    } else {
+        komentar = "Jumlah Pria dan Wanita berimbang"
+    }
+    console.log(komentar);
 
-  // ! JANGAN DIMODIFIKASI
-  return {
-    hasilLooping,
-    jumlahPria,
-    jumlahWanita,
-    komentar,
-  };
+    // ! JANGAN DIMODIFIKASI
+    return {
+        hasilLooping,
+        jumlahPria,
+        jumlahWanita,
+        komentar,
+    };
 }
 
 function main(data) {
-  const hasil = lakukanLooping(data || arrayObjectPegawai);
+    const hasil = lakukanLooping(data || arrayObjectPegawai);
 
-  console.log(hasil.hasilLooping);
-  console.log(hasil.jumlahPria);
-  console.log(hasil.jumlahWanita);
+    console.log(hasil.hasilLooping);
+    console.log(hasil.jumlahPria);
+    console.log(hasil.jumlahWanita);
 
-  return hasil;
+    return hasil;
 }
 
 main(arrayObjectPegawai);
